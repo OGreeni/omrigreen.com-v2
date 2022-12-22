746a0b0f3d1d782096bc2b7a6f015a140f4d8150
OGreeni <omri.green1@gmail.com>
2022-12-21 23:27:06 -0500

    add styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-400': 'var(--color-primary-400)',
+        'neutral-400': 'var(--color-neutral-400)',
+      },
+    },
+  },
+  plugins: [],
+};
